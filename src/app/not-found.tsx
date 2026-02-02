1d4c27d6d65fc03fab5d7f52049fa4f3e166f63b
SergWebAcceptance <serg.web@acceptance.ltd>
2026-02-02 12:39:00 +0200

    Refactor NotFound component: Simplify the NotFound page by removing unnecessary wrappers and directly rendering the NotFoundSection. This improves code clarity and reduces complexity.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,20 +1,7 @@
-import { NextIntlClientProvider } from "next-intl";
 
 import { NotFoundSection } from "@/features/not-found/ui/NotFoundSection/NotFoundSection";
-
-import { Footer } from "@/shared/ui/components/footer/Footer";
-import { Header } from "@/shared/ui/components/header/Header";
-
 const NotFound = () => {
-  return (
-    <>
-    <NextIntlClientProvider>
-      <Header />
-      <NotFoundSection />
-      <Footer />
-    </NextIntlClientProvider>
-    </>
-  );
-}
+  return <NotFoundSection />;
+};
 
 export default NotFound;
